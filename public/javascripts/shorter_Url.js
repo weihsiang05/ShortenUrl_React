4bbb71c5ab1cb738d41603604554e9848adcd9db
weihsiang05 <weihsiang0000@gmail.ocm>
2023-09-13 18:29:29 -0400

    Create five random characters to shorten the original link.

--- public/javascripts/shorter_Url.js
+++ public/javascripts/shorter_Url.js
@@ -1,12 +1,17 @@
 function link(shorter) {
-  return shorter.link
-  //const randomletter = [ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890]
-  //let createRandomletter = "";
+  const randomletter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  const fixLink = "https:// .../"
+  let createRandomletter = ""
+  let newLink = ""
 
-  // for (let i = 0; i < 5; i++) {
-  //   Math.floor(Math.random() * randomletter.length)
-  // }
-  //console.log(shorter.link);
+  for (let i = 0; i < 5; i++) {
+    let random = Math.floor(Math.random() * randomletter.length)
+    createRandomletter += randomletter[random]
+  }
+  newLink = fixLink + createRandomletter
+
+  return newLink
 }
+
 
 module.exports = { link }
