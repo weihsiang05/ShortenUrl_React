8294be3fef8f9230aee3945d573de667a8a16853
weihsiang05 <weihsiang0000@gmail.ocm>
2023-09-13 17:59:22 -0400

    Render the success page

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
 const port = 3000
+const shorterURL = require('./public/javascripts/shorter_Url')
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.redirect('/link')
@@ -17,6 +19,11 @@ app.get('/link', (req, res) => {
   res.render('index')
 })
 
+app.post('/link', (req, res) => {
+  const originalLink = req.body
+  let shorterLink = shorterURL.link(originalLink);
+  res.render('success', { shorterLink })
+})
 
 app.listen(port, () => {
   console.log(`express server is running on the http://localhost:${port}`)
---
+++ public/javascripts/shorter_Url.js
@@ -0,0 +1,12 @@
+function link(shorter) {
+  return shorter.link
+  //const randomletter = [ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890]
+  //let createRandomletter = "";
+
+  // for (let i = 0; i < 5; i++) {
+  //   Math.floor(Math.random() * randomletter.length)
+  // }
+  //console.log(shorter.link);
+}
+
+module.exports = { link }
